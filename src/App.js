279fcb726d35f1dc700311d92f524f7d61f0b49b
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2021-09-10 11:04:59 +0900

    css style basic done

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-container">
+        <header className="App-header">
+          <h1>Login Mini Project</h1>
+        </header>
+        <section className="App-section">
+          <div className="input-box">
+            <label>User Name</label>
+            <input name="housename" type="text" placeholder="User Name" required autofocus />
+            <label>Password</label>
+            <input name="password" type="password" placeholder="Password" required />
+            <input type="button" value="Login" />
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
