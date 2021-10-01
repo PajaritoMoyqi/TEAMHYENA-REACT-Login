f7581525fa32feed0b1bdfcf35fe18c51fe56d8b
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2021-10-01 17:38:12 +0900

    routing error fixing 2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Join from './Join';
 import Main from './Main';
-import { Route } from 'react-router-dom';
+import { Route, BrowserRouter } from 'react-router-dom';
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Route exact path="/" component={Main} />
       <Route path="/join" component={Join} />
-    </>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/TEAMHYENA-REACT-Login">
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App />
     </BrowserRouter>
   </React.StrictMode>
