4ae64ff94802379ac4126b6c8ae2fab8ead15894
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2021-10-03 20:41:53 +0900

    routing error fixing 6

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Join from './Join';
 import Main from './Main';
-import { Route, BrowserRouter } from 'react-router-dom';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 
 const App = () => {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Route exact path="/" component={Main} />
-      <Route path="/join/" component={Join} />
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route path="/join/" component={Join} />
+      </Switch>
     </BrowserRouter>
   );
 }
