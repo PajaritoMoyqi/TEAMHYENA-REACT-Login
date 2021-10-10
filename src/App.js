ee9c1ee819541f7f720208c01077b88c1e618e86
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2021-10-10 16:49:44 +0900

    routing error fixing 7

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Join from './Join';
 import Main from './Main';
-import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import { Route, BrowserRouter, Switch, Link } from 'react-router-dom';
 
 const App = () => {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Link to="/">홈</Link>
+      <Link to="/join">회원가입</Link>
       <Switch>
         <Route exact path="/"><Main /></Route>
-        <Route path="/join/"><Join /></Route>
+        <Route path="/join"><Join /></Route>
       </Switch>
     </BrowserRouter>
   );
